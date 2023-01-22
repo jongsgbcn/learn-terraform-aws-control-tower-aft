249d40dade540e9ce638c71d242a004d1f0d5f8b
Jon Nmaju <jon.nmaju@sandbox.local>
2023-01-22 14:05:51 +0100

    add varibales, run plan until succes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "aft" {
   ct_home_region              = var.ct_home_region
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
-  region = var.ct_home_region
+ 
   vcs_provider                                  = "github"
   account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
   account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
